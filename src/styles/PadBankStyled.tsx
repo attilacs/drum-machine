f1fb79618f58f93a6dd560e3aa435dda05685624
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-06 15:50:40 +0100

    Create styles for pad bank

---
+++ src/styles/PadBankStyled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const PadBankStyled = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-gap: 15px;
+  margin-bottom: 30px;
+`;
+
+export default PadBankStyled;
