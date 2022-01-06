be458df0e08215309bcc9d9c142a8eeb62ef4a1b
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-06 15:47:19 +0100

    Create drum pad list

---
+++ src/components/drumPadList.ts
@@ -0,0 +1,49 @@
+const drumPads = [
+  {
+    button: "Q",
+    keyCode: 81,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+  {
+    button: "W",
+    keyCode: 87,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+  {
+    button: "E",
+    keyCode: 69,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+  {
+    button: "A",
+    keyCode: 65,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+  {
+    button: "S",
+    keyCode: 83,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+  {
+    button: "D",
+    keyCode: 68,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+  {
+    button: "Z",
+    keyCode: 90,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+  {
+    button: "X",
+    keyCode: 88,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+  {
+    button: "C",
+    keyCode: 67,
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+];
+
+export default drumPads;
