26a921b8838ccb61b9497ef4a404e2a8dc12d068
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-06 15:38:14 +0100

    Create default colors file

---
+++ src/styles/colors.ts
@@ -0,0 +1,6 @@
+export const buttonColor = "#325FDA";
+export const containerBackground = "#F5F7FD";
+export const displayColor = "#1A398E";
+export const labelColor = containerBackground;
+export const pageBackground = "#8BA4EA";
+export const textColor = "#101930";
