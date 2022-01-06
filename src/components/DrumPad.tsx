0b5a994edc41152abb3427b5cf5a39715670eb67
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-06 16:12:30 +0100

    Create play sound function

--- src/components/DrumPad.tsx
+++ src/components/DrumPad.tsx
@@ -14,10 +14,19 @@ interface DrumPadProps {
 const DrumPad = ({ setDisplayText, drumPad }: DrumPadProps) => {
   const { button, keyCode, url } = drumPad;
 
+  const playSound = () => {
+    const audio = document.querySelector<HTMLAudioElement>(`#${button}`);
+    if (audio) {
+      audio.currentTime = 0;
+      audio.play();
+    }
+  };
+
   return (
     <DrumPadStyled
       className="drum-pad"
       id={`pad-${button}`}
+      onClick={playSound}
     >
       {button}
       <audio className="clip" src={url} id={button} />
