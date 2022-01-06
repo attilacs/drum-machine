2f43f06325debc8322da18d3806658297c90d359
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-06 16:00:15 +0100

    Create state for display text

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,7 +1,10 @@
 import ContainerStyled from "../styles/ContainerStyled";
 import Heading from "./Heading";
+import { useState } from "react";
 
 const Container = () => {
+  const [displayText, setDisplayText] = useState("");
+
   return (
     <ContainerStyled id="drum-machine">
       <Heading />
